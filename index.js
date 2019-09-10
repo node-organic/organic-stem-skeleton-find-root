292b5d903768b70ebd9e0ed9ce91760f1806b2b1
outbounder <obiwonn@gmail.com>
2019-09-10 21:43:18 +0300

    fix usage without find-up results

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ const findUp = require('find-up')
 
 module.exports = async function () {
   let absolute_path = await findUp('dna', {cwd: path.dirname(process.cwd())})
+  if (absolute_path === null) {
+    // no root path found, return cwd by default
+    return process.cwd()
+  }
   return path.dirname(absolute_path)
 }
